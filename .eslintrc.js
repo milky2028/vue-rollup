bede1d3fb461451eef3398dcde4072a7eb72b6a9
Tyler Gross <tylergross28@outlook.com>
2019-12-17 19:11:54 +0000

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,31 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/recommended', '@vue/prettier', '@vue/typescript'],
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/prettier',
+    '@vue/typescript'
+  ],
   rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always'
+        }
+      }
+    ],
+    '@typescript-eslint/semi': [
+      'error',
+      'always',
+      { omitLastInOneLineBlock: true }
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-useless-catch': 'error',
+    'arrow-parens': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },

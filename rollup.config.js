9f982f552fbddc1bd38fd5b747b6d15a0e08b1d1
Tyler Gross <tylergross28@outlook.com>
2020-01-10 23:38:48 +0000

    minimal working=

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import vue from 'rollup-plugin-vue';
 import typescript from '@rollup/plugin-typescript';
-// import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import rimraf from 'rimraf';
-// import babel from 'rollup-plugin-babel';
-// import postcss from 'rollup-plugin-postcss';
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
 import progress from 'rollup-plugin-progress';
 import environmentPlugin from './lib/environment-plugin';
 import html from '@rollup/plugin-html';
@@ -38,17 +38,19 @@ export default {
     environmentPlugin(environment),
     typescript(),
     vue(),
-    // babel({
-    //   extensions,
-    //   presets: ['@vue/cli-plugin-babel/preset'],
-    //   runtimeHelpers: true,
-    //   include: ['src/**/*'],
-    //   exclude: ['node_modules/**']
-    // }),
-    // postcss({ modules: true, minimize: true }),
+    babel({
+      extensions,
+      presets: ['@vue/cli-plugin-babel/preset'],
+      runtimeHelpers: true,
+      include: ['src/**/*'],
+      exclude: ['node_modules/**']
+    }),
     replace({ 'process.env.NODE_ENV': `'production'`, 'process.env.BASE_URL': `'/'`}),
-    html(),
+    html({ attributes: { html: { lang: 'en' } } }),
     // css({ chunkFileNames: 'chunk-[hash].css', entryFileNames: '[name]-[hash].css' }),
-    // terser()
+    postcss({ minimize: true }),
+    terser()
   ]
 };
+
+// In output, resulting index.html needs an 'app' id to mount the app. CSS chunking does not appear to work. Environment plugin needs to be tested. Is not currently working.

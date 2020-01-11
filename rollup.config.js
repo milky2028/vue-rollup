2d3d0e91c54ee6a418be456a4f8a94984ba7f3f0
Tyler Gross <tylergross28@gmail.com>
2020-01-10 22:44:03 -0600

    rollup config with html minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,57 @@
-import vue from 'rollup-plugin-vue';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-import rimraf from 'rimraf';
-import babel from 'rollup-plugin-babel';
-import postcss from 'rollup-plugin-postcss';
-import progress from 'rollup-plugin-progress';
-import environmentPlugin from './lib/environment-plugin';
-import html from '@rollup/plugin-html';
-import replace from '@rollup/plugin-replace';
-import commonjs from '@rollup/plugin-commonjs';
+import vue from "rollup-plugin-vue";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import rimraf from "rimraf";
+import babel from "rollup-plugin-babel";
+import postcss from "rollup-plugin-postcss";
+import progress from "rollup-plugin-progress";
+import environmentPlugin from "./lib/environment-plugin";
+import html, { makeHtmlAttributes } from "@rollup/plugin-html";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
 // import css from 'rollup-plugin-css-chunks';
-import { readFileSync } from 'fs';
+import { minify } from "html-minifier";
+import { readFileSync } from "fs";
 
 if (!process.env.MODE) {
-  throw new Error('No environment mode specified');
+  throw new Error("No environment mode specified");
 }
 
-rimraf.sync('rollup-dist');
-const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+function createTemplate({ attributes, files, publicPath }) {
+  const scripts = (files.js || [])
+    .map(({ fileName }) => {
+      const attrs = makeHtmlAttributes(attributes.script);
+      return `<script src="${publicPath}${fileName}"${attrs}></script>`;
+    })
+    .join("\n");
+  return minify(
+    readFileSync("./public/index.html", "utf-8").replace("${scripts}", scripts),
+    {
+      removeAttributeQuotes: true,
+      removeComments: true,
+      collapseInlineTagWhitespace: true,
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      removeEmptyAttributes: true
+    }
+  );
+}
+
+rimraf.sync("rollup-dist");
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 const environment = readFileSync(
   `./environments/${process.env.MODE}.ts`,
-  'utf-8'
+  "utf-8"
 );
 export default {
-  input: 'src/main.ts',
+  input: "src/app/main.ts",
   output: {
-    dir: 'rollup-dist',
-    format: 'esm',
-    entryFileNames: '[name]-[hash].js',
-    chunkFileNames: '[name]-[hash].js'
+    dir: "rollup-dist",
+    format: "esm",
+    entryFileNames: "[name]-[hash].js",
+    chunkFileNames: "[name]-[hash].js"
   },
   plugins: [
     progress(),
@@ -40,17 +62,25 @@ export default {
     vue(),
     babel({
       extensions,
-      presets: ['@vue/cli-plugin-babel/preset'],
+      presets: ["@vue/cli-plugin-babel/preset"],
       runtimeHelpers: true,
-      include: ['src/**/*'],
-      exclude: ['node_modules/**']
+      include: ["src/**/*"],
+      exclude: ["node_modules/**"]
     }),
-    replace({ 'process.env.NODE_ENV': `'production'`, 'process.env.BASE_URL': `'/'`}),
-    html({ attributes: { html: { lang: 'en' } } }),
+    replace({
+      "process.env.NODE_ENV": `'production'`,
+      "process.env.BASE_URL": `'/'`
+    }),
+    html({
+      attributes: {
+        html: {
+          lang: "en"
+        }
+      },
+      template: createTemplate
+    }),
     // css({ chunkFileNames: 'chunk-[hash].css', entryFileNames: '[name]-[hash].css' }),
     postcss({ minimize: true }),
     terser()
   ]
 };
-
-// In output, resulting index.html needs an 'app' id to mount the app. CSS chunking does not appear to work. Environment plugin needs to be tested. Is not currently working.

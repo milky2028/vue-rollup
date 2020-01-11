75831fe257eb866da306e2015cdba5938cdac768
Tyler Gross <tylergross28@gmail.com>
2020-01-10 22:51:05 -0600

    changes so we can build and compare

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
- 
 module.exports = {
   root: true,
   env: {
     node: true
   },
   extends: [
-    'plugin:vue/recommended',
     'eslint:recommended',
+    'plugin:vue/recommended',
     '@vue/prettier',
     '@vue/typescript/recommended',
     '@vue/prettier/@typescript-eslint'
@@ -16,6 +15,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'vue/html-self-closing': [
       'error',
       {
@@ -29,12 +29,16 @@ module.exports = {
       'always',
       { omitLastInOneLineBlock: true }
     ],
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, arrowParens: 'always' }
+    ],
     quotes: ['error', 'single', { avoidEscape: true }],
     'no-shadow': ['error', { builtinGlobals: true, hoist: 'all' }],
     'no-useless-catch': 'error',
     'arrow-parens': ['error', 'always'],
     'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {

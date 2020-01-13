397df97cad9bae978d64f978c1e4077feb956d1a
Tyler <tgross@skyroad.com>
2020-01-13 17:02:29 -0600

    lazy loaded components don't work

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+const Home = () => import('../views/Home.vue');
+const About = () => import('../views/About.vue');
 
 Vue.use(VueRouter);
 
@@ -16,12 +17,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue')
+    component: About
   }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 });
